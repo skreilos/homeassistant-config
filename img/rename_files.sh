330d5cd35ad11ccb7a3fe1ef41c7d00be29596ba
Kreilos Stephan <mail@kreilos.ch>
2026-04-26 15:05:44 +0200

    Initial Home Assistant config snapshot

---
+++ img/rename_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+rm img/door20.jpg
+mv img/door19.jpg img/door20.jpg
+mv img/door18.jpg img/door19.jpg
+mv img/door17.jpg img/door18.jpg
+mv img/door16.jpg img/door17.jpg
+mv img/door15.jpg img/door16.jpg
+mv img/door14.jpg img/door15.jpg
+mv img/door13.jpg img/door14.jpg
+mv img/door12.jpg img/door13.jpg
+mv img/door11.jpg img/door12.jpg
+mv img/door10.jpg img/door11.jpg
+mv img/door09.jpg img/door10.jpg
+mv img/door08.jpg img/door09.jpg
+mv img/door07.jpg img/door08.jpg
+mv img/door06.jpg img/door07.jpg
+mv img/door05.jpg img/door06.jpg
+mv img/door04.jpg img/door05.jpg
+mv img/door03.jpg img/door04.jpg
+mv img/door02.jpg img/door03.jpg
+mv img/door01.jpg img/door02.jpg
+cp img/door.jpg img/door01.jpg
+
